248da6e8dd54f5f7129f7ecc7368f42bcc37a73b
Muhammad Saim <Saim_DXB@outlook.com>
2023-11-19 18:56:08 +0400

    changed products into lessons

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        "id": 1,
+        "subject": "Robotics",
+        "image":"robotics.png",
+        "location": "Room 101",
+        "price": 100,
+        "spaces": 5,
+        "rating": 0
+    },
+    {
+        "id": 2,
+        "subject": "Gymnastics",
+        "image":"gymnastics.png",
+        "location": "Room 111",
+        "price": 125,
+        "spaces": 6,
+        "rating": 0
+    },
+    {
+        "id": 3,
+        "subject": "Athletics",
+        "image":"athletics.png",
+        "location": "Room 123",
+        "price": 210,
+        "spaces": 7,
+        "rating": 0
+    },
+    {
+        "id": 4,
+        "subject": "Chess",
+        "image":"chess.png",
+        "location": "Room 112",
+        "price": 155,
+        "spaces": 8,
+        "rating": 0
+    },
+    {
+        "id": 5,
+        "subject": "Karate",
+        "image":"karate.png",
+        "location": "Room 100",
+        "price": 175,
+        "spaces": 7,
+        "rating": 0
+    },
+    {
+        "id": 6,
+        "subject": "Swimming",
+        "image":"swimming.png",
+        "location": "Room 130",
+        "price": 103,
+        "spaces": 9,
+        "rating": 0
+    },
+    {
+        "id": 7,
+        "subject": "Volleyball",
+        "image":"volleyball.png",
+        "location": "Room 190",
+        "price": 170,
+        "spaces": 5,
+        "rating": 0
+    },
+    {
+        "id": 8,
+        "subject": "Football",
+        "image":"football.png",
+        "location": "Room 172",
+        "price": 110,
+        "spaces": 8,
+        "rating": 0
+    },
+    {
+        "id": 9,
+        "subject": "Tennis",
+        "image":"tennis.png",
+        "location": "Room 131",
+        "price": 190,
+        "spaces": 10,
+        "rating": 0
+    },
+    {
+        "id": 10,
+        "subject": "Archery",
+        "image":"archery.png",
+        "location": "Room 191",
+        "price": 180,
+        "spaces": 11,
+        "rating": 0
+    }
+];
